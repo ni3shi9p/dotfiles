bdd2b1c991fa70e00f7bca01a7598f59b6fc010c
k-nishigaki <k-nishigaki@nekojarashi.com>
2016-02-26 13:16:24 +0900

    modify link

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,6 +4,6 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/filetype.vim ~/.vim/filetype.vim
-ln -sf ~/dotfiles/.vim/ftplugin/ ~/.vim/ftplugin/
+ln -sf ~/dotfiles/.vim/ftplugin/ ~/.vim/ftplugin
 
 #cp -r ~/dotfiles/.gitconfig ~/.gitconfig
